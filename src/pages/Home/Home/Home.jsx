8fa0d208e681215d348fdef46a5d4b56d659fa35
shejanNizam <nizam.shejan11@gmail.com>
2024-10-31 04:50:23 +0600

    services section added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import About from "../About/About";
 import Banner from "../Banner/Banner";
+import Services from "../Services/Services";
 
 export default function Home() {
   return (
     <>
       <Banner />
       <About />
+      <Services />
     </>
   );
 }
