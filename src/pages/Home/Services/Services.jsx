6fdb91598ae77250508d2af0019e42c3c6cf73b1
shejanNizam <nizam.shejan11@gmail.com>
2024-11-04 09:47:21 +0600

    updated text

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -17,9 +17,10 @@ export default function Services() {
           <h3 className="text-3xl font-bold text-orange-500">Our Services</h3>
           <h2 className="text-5xl font-bold py-4">Our Service Area</h2>
           <p>
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised <br /> words which do not look even slightly
-            believable.{" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+            quaerat tempora voluptatem, <br /> asperiores reiciendis aut
+            praesentium illo repellendus tenetur repellat nesciunt deleniti
+            nulla nihil eaque esse delectus est iure itaque!
           </p>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
