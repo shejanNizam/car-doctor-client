da2e1c96388a3338e7c51259751b67d111bfbdbc
shejanNizam <nizam.shejan11@gmail.com>
2024-11-04 18:18:28 +0600

    create single data booking

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,11 +1,103 @@
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
 
 export default function Checkout() {
   const service = useLoaderData();
-  const { title } = service;
+  const { _id, title, price, service_id, img } = service;
+
+  const { user } = useContext(AuthContext);
+
+  const handleBookService = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const name = form.name.value;
+    const date = form.date.value;
+    const email = user?.email;
+    const booking = {
+      customerName: name,
+      email,
+      img,
+      date,
+      service: title,
+      service_id: _id,
+      price,
+    };
+    console.log(booking);
+
+    fetch("http://localhost:7000/bookings", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(booking),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          alert("service book successfully");
+        }
+      });
+  };
+
   return (
     <>
-      <h2>Checkout Page for: {title} </h2>
+      <div>
+        <h2 className="text-center text-3xl">Book Service: {title} </h2>
+        <form onSubmit={handleBookService}>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                defaultValue={user?.displayName}
+                name="name"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Date</span>
+              </label>
+              <input type="date" name="date" className="input input-bordered" />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                name="email"
+                defaultValue={user?.email}
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Due amount</span>
+              </label>
+              <input
+                type="text"
+                defaultValue={"$" + price}
+                className="input input-bordered"
+              />
+            </div>
+          </div>
+          <div className="form-control mt-6">
+            <input
+              className="btn btn-primary btn-block"
+              type="submit"
+              value="Order Confirm"
+            />
+          </div>
+        </form>
+        <div className="card-body"></div>
+      </div>
     </>
   );
 }
